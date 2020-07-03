21c04614b7cdfb5c3ceb0c4c291ee0c78dbf21e1
Ryan Newington <ryan@lithiumblue.com>
2020-07-03 10:46:30 +1000

    Targets .net core

--- src/SslCertBinding.Net/HttpApi.cs
+++ src/SslCertBinding.Net/HttpApi.cs
@@ -4,35 +4,35 @@ using System.Runtime.InteropServices;
 
 namespace SslCertBinding.Net
 {
-	internal class HttpApi
-	{
-		// ReSharper disable InconsistentNaming
+    internal class HttpApi
+    {
+        // ReSharper disable InconsistentNaming
 
-		public static void ThrowWin32ExceptionIfError(uint retVal) {
-			if (NO_ERROR != retVal) {
-				throw new Win32Exception(Convert.ToInt32(retVal));
-			}
-		}
+        public static void ThrowWin32ExceptionIfError(uint retVal) {
+            if (NO_ERROR != retVal) {
+                throw new Win32Exception(Convert.ToInt32(retVal));
+            }
+        }
 
-		public static void CallHttpApi(Action body) {
-			uint retVal = HttpInitialize(HttpApiVersion, HTTP_INITIALIZE_CONFIG, IntPtr.Zero);
-			ThrowWin32ExceptionIfError(retVal);
+        public static void CallHttpApi(Action body) {
+            uint retVal = HttpInitialize(HttpApiVersion, HTTP_INITIALIZE_CONFIG, IntPtr.Zero);
+            ThrowWin32ExceptionIfError(retVal);
 
-			try {
-				body();
-			} finally {
-				HttpTerminate(HTTP_INITIALIZE_CONFIG, IntPtr.Zero);
-			}
-		}
+            try {
+                body();
+            } finally {
+                HttpTerminate(HTTP_INITIALIZE_CONFIG, IntPtr.Zero);
+            }
+        }
 
-		public delegate void Action();
+        public delegate void Action();
 
-		#region DllImport
+        #region DllImport
 
-		[DllImport("httpapi.dll", SetLastError = true)]
-		public static extern uint HttpInitialize(
-			HTTPAPI_VERSION version,
-			uint flags,
+        [DllImport("httpapi.dll", SetLastError = true)]
+        public static extern uint HttpInitialize(
+            HTTPAPI_VERSION version,
+            uint flags,
             IntPtr pReserved);
 
         [DllImport("httpapi.dll", SetLastError = true)]
@@ -67,9 +67,9 @@ namespace SslCertBinding.Net
             int configInformationLength,
             IntPtr pOverlapped);
 
-		[DllImport("httpapi.dll", SetLastError = true)]
-		public static extern uint HttpTerminate(
-			uint Flags,
+        [DllImport("httpapi.dll", SetLastError = true)]
+        public static extern uint HttpTerminate(
+            uint Flags,
             IntPtr pReserved);
 
         [DllImport("httpapi.dll", SetLastError = true)]
@@ -81,16 +81,16 @@ namespace SslCertBinding.Net
                 IntPtr pOutputConfigInfo,
                 int outputConfigInfoLength,
                 [Optional]
-				out int pReturnLength,
+                out int pReturnLength,
                 IntPtr pOverlapped);
 
-		public enum HTTP_SERVICE_CONFIG_ID
-		{
-			HttpServiceConfigIPListenList = 0,
-			HttpServiceConfigSSLCertInfo,
-			HttpServiceConfigUrlAclInfo,
-			HttpServiceConfigMax
-		}
+        public enum HTTP_SERVICE_CONFIG_ID
+        {
+            HttpServiceConfigIPListenList = 0,
+            HttpServiceConfigSSLCertInfo,
+            HttpServiceConfigUrlAclInfo,
+            HttpServiceConfigMax
+        }
 
         [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
         public struct HTTP_SERVICE_CONFIG_URLACL_KEY
@@ -106,7 +106,7 @@ namespace SslCertBinding.Net
             public string pStringSecurityDescriptor;
         }
 
-		[StructLayout(LayoutKind.Sequential)]
+        [StructLayout(LayoutKind.Sequential)]
         public struct HTTP_SERVICE_CONFIG_URLACL_SET
         {
             public HTTP_SERVICE_CONFIG_URLACL_KEY KeyDesc;
@@ -121,134 +121,134 @@ namespace SslCertBinding.Net
             public Int32 dwToken;
         }
 
-		[StructLayout(LayoutKind.Sequential)]
-		public struct HTTP_SERVICE_CONFIG_SSL_SET
-		{
-			public HTTP_SERVICE_CONFIG_SSL_KEY KeyDesc;
-			public HTTP_SERVICE_CONFIG_SSL_PARAM ParamDesc;
-		}
+        [StructLayout(LayoutKind.Sequential)]
+        public struct HTTP_SERVICE_CONFIG_SSL_SET
+        {
+            public HTTP_SERVICE_CONFIG_SSL_KEY KeyDesc;
+            public HTTP_SERVICE_CONFIG_SSL_PARAM ParamDesc;
+        }
 
-		[StructLayout(LayoutKind.Sequential)]
-		public struct HTTP_SERVICE_CONFIG_SSL_KEY
-		{
-			public IntPtr pIpPort;
+        [StructLayout(LayoutKind.Sequential)]
+        public struct HTTP_SERVICE_CONFIG_SSL_KEY
+        {
+            public IntPtr pIpPort;
 
-			public HTTP_SERVICE_CONFIG_SSL_KEY(IntPtr pIpPort) {
-				this.pIpPort = pIpPort;
-			}
-		}
+            public HTTP_SERVICE_CONFIG_SSL_KEY(IntPtr pIpPort) {
+                this.pIpPort = pIpPort;
+            }
+        }
 
-		[StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
-		public struct HTTP_SERVICE_CONFIG_SSL_PARAM
-		{
-			public int SslHashLength;
-			public IntPtr pSslHash;
-			public Guid AppId;
-			[MarshalAs(UnmanagedType.LPWStr)]
-			public string pSslCertStoreName;
-			public CertCheckModes DefaultCertCheckMode;
-			public int DefaultRevocationFreshnessTime;
-			public int DefaultRevocationUrlRetrievalTimeout;
-			[MarshalAs(UnmanagedType.LPWStr)]
-			public string pDefaultSslCtlIdentifier;
-			[MarshalAs(UnmanagedType.LPWStr)]
-			public string pDefaultSslCtlStoreName;
-			public HTTP_SERVICE_CONFIG_SSL_FLAG DefaultFlags;
-		}
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        public struct HTTP_SERVICE_CONFIG_SSL_PARAM
+        {
+            public int SslHashLength;
+            public IntPtr pSslHash;
+            public Guid AppId;
+            [MarshalAs(UnmanagedType.LPWStr)]
+            public string pSslCertStoreName;
+            public CertCheckModes DefaultCertCheckMode;
+            public int DefaultRevocationFreshnessTime;
+            public int DefaultRevocationUrlRetrievalTimeout;
+            [MarshalAs(UnmanagedType.LPWStr)]
+            public string pDefaultSslCtlIdentifier;
+            [MarshalAs(UnmanagedType.LPWStr)]
+            public string pDefaultSslCtlStoreName;
+            public HTTP_SERVICE_CONFIG_SSL_FLAG DefaultFlags;
+        }
 
-		[StructLayout(LayoutKind.Sequential, Pack = 2)]
-		public struct HTTPAPI_VERSION
-		{
-			public ushort HttpApiMajorVersion;
-			public ushort HttpApiMinorVersion;
+        [StructLayout(LayoutKind.Sequential, Pack = 2)]
+        public struct HTTPAPI_VERSION
+        {
+            public ushort HttpApiMajorVersion;
+            public ushort HttpApiMinorVersion;
 
-			public HTTPAPI_VERSION(ushort majorVersion, ushort minorVersion) {
-				HttpApiMajorVersion = majorVersion;
-				HttpApiMinorVersion = minorVersion;
-			}
-		}
+            public HTTPAPI_VERSION(ushort majorVersion, ushort minorVersion) {
+                HttpApiMajorVersion = majorVersion;
+                HttpApiMinorVersion = minorVersion;
+            }
+        }
 
-		[StructLayout(LayoutKind.Sequential)]
-		public struct HTTP_SERVICE_CONFIG_SSL_QUERY
-		{
-			public HTTP_SERVICE_CONFIG_QUERY_TYPE QueryDesc;
-			public HTTP_SERVICE_CONFIG_SSL_KEY KeyDesc;
-			public uint dwToken;
-		}
+        [StructLayout(LayoutKind.Sequential)]
+        public struct HTTP_SERVICE_CONFIG_SSL_QUERY
+        {
+            public HTTP_SERVICE_CONFIG_QUERY_TYPE QueryDesc;
+            public HTTP_SERVICE_CONFIG_SSL_KEY KeyDesc;
+            public uint dwToken;
+        }
 
-		public enum HTTP_SERVICE_CONFIG_QUERY_TYPE
-		{
-			HttpServiceConfigQueryExact = 0,
-			HttpServiceConfigQueryNext,
-			HttpServiceConfigQueryMax
-		}
+        public enum HTTP_SERVICE_CONFIG_QUERY_TYPE
+        {
+            HttpServiceConfigQueryExact = 0,
+            HttpServiceConfigQueryNext,
+            HttpServiceConfigQueryMax
+        }
 
-		/// <summary>
-		/// Determines how client certificates are checked. 
-		/// </summary>
-		[Flags]
-		public enum CertCheckModes : uint
-		{
-			/// <summary>
-			/// Enables the client certificate revocation check.
-			/// </summary>
-			None = 0,
+        /// <summary>
+        /// Determines how client certificates are checked. 
+        /// </summary>
+        [Flags]
+        public enum CertCheckModes : uint
+        {
+            /// <summary>
+            /// Enables the client certificate revocation check.
+            /// </summary>
+            None = 0,
 
-			/// <summary>
-			/// Client certificate is not to be verified for revocation. 
-			/// </summary>
-			DoNotVerifyCertificateRevocation = 1,
+            /// <summary>
+            /// Client certificate is not to be verified for revocation. 
+            /// </summary>
+            DoNotVerifyCertificateRevocation = 1,
 
-			/// <summary>
-			/// Only cached certificate is to be used the revocation check. 
-			/// </summary>
-			VerifyRevocationWithCachedCertificateOnly = 2,
+            /// <summary>
+            /// Only cached certificate is to be used the revocation check. 
+            /// </summary>
+            VerifyRevocationWithCachedCertificateOnly = 2,
 
-			/// <summary>
-			/// The RevocationFreshnessTime setting is enabled.
-			/// </summary>
-			EnableRevocationFreshnessTime = 4,
+            /// <summary>
+            /// The RevocationFreshnessTime setting is enabled.
+            /// </summary>
+            EnableRevocationFreshnessTime = 4,
 
-			/// <summary>
-			/// No usage check is to be performed.
-			/// </summary>
-			NoUsageCheck = 0x10000
-		}
+            /// <summary>
+            /// No usage check is to be performed.
+            /// </summary>
+            NoUsageCheck = 0x10000
+        }
 
-		[Flags]
-		public enum HTTP_SERVICE_CONFIG_SSL_FLAG : uint
-		{
-			NONE = 0,
+        [Flags]
+        public enum HTTP_SERVICE_CONFIG_SSL_FLAG : uint
+        {
+            NONE = 0,
 
-			/// <summary>
-			/// Client certificates are mapped where possible to corresponding operating-system user accounts based on the certificate mapping rules stored in Active Directory.
-			/// </summary>
-			USE_DS_MAPPER = 0x00000001,
+            /// <summary>
+            /// Client certificates are mapped where possible to corresponding operating-system user accounts based on the certificate mapping rules stored in Active Directory.
+            /// </summary>
+            USE_DS_MAPPER = 0x00000001,
 
-			/// <summary>
-			/// Enables a client certificate to be cached locally for subsequent use.
-			/// </summary>
-			NEGOTIATE_CLIENT_CERT = 0x00000002,
+            /// <summary>
+            /// Enables a client certificate to be cached locally for subsequent use.
+            /// </summary>
+            NEGOTIATE_CLIENT_CERT = 0x00000002,
 
-			/// <summary>
-			/// Prevents SSL requests from being passed to low-level ISAPI filters.
-			/// </summary>
-			NO_RAW_FILTER = 0x00000004,
-		}
+            /// <summary>
+            /// Prevents SSL requests from being passed to low-level ISAPI filters.
+            /// </summary>
+            NO_RAW_FILTER = 0x00000004,
+        }
 
-		#endregion
+        #endregion
 
-		#region Constants
+        #region Constants
 
-		public const uint HTTP_INITIALIZE_CONFIG = 0x00000002;
-		public const uint NO_ERROR = 0;
-		public const uint ERROR_INSUFFICIENT_BUFFER = 122;
-		public const uint ERROR_ALREADY_EXISTS = 183;
-		public const uint ERROR_FILE_NOT_FOUND = 2;
-		public const int ERROR_NO_MORE_ITEMS = 259;
+        public const uint HTTP_INITIALIZE_CONFIG = 0x00000002;
+        public const uint NO_ERROR = 0;
+        public const uint ERROR_INSUFFICIENT_BUFFER = 122;
+        public const uint ERROR_ALREADY_EXISTS = 183;
+        public const uint ERROR_FILE_NOT_FOUND = 2;
+        public const int ERROR_NO_MORE_ITEMS = 259;
 
-		#endregion
+        #endregion
 
-		public static readonly HTTPAPI_VERSION HttpApiVersion = new HTTPAPI_VERSION(1, 0);
-	}
+        public static readonly HTTPAPI_VERSION HttpApiVersion = new HTTPAPI_VERSION(1, 0);
+    }
 }
